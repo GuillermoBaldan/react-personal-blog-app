e957a5095e70ed3d67ee0fd279ae810c9099ab43
guillermo <guillermobaldan@gmail.com>
2024-08-09 12:24:45 +0200

    solving routing problems

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import BlogList from './components/BlogList';
 import BlogPost from './components/BlogPost';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<BlogList />} />
-          <Route path="/post/:id" element={<BlogPost />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<BlogList />} />
+        <Route path="/post/:id" element={<BlogPost />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
+      </Routes>
+    </div>
   );
 }
 
