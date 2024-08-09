77943bb2f255a9f475fb3b40f445ab46f24f000e
guillermo <guillermobaldan@gmail.com>
2024-08-09 08:11:40 +0200

    modifications on App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BlogList from './components/BlogList';
 import BlogPost from './components/BlogPost';
-import './App.css';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={BlogList} />
-          <Route path="/post/:id" component={BlogPost} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<BlogList />} />
+          <Route path="/post/:id" element={<BlogPost />} />
+        </Routes>
       </div>
     </Router>
   );
